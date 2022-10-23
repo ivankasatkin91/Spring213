96b7ad15ed2e71ebedb733fbb4cde03043fc5d13
ivankasatkin91 <ivankasatkin91@gmail.com>
2022-10-23 21:31:06 +0300

    release v 1.00: Setting autowared beans by name

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,16 @@
 package app.model;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
 
+    @Bean
+    @Scope("Singleton") // Bean scope is "Singleton" by default - explicitly annotated in terms of good practice
     public Long getTime() {
         return nanoTime;
     }
